d21d6f8fdc470071c0a5cc398aa946d2c6704557
Minhazul Alam <78158511+akib-chy@users.noreply.github.com>
2022-04-16 19:37:26 +0600

    first commit

---
+++ src/Page/Form/Login/Login.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Login = () => {
+  return (
+    <div>
+      <h2>This si login section</h2>
+    </div>
+  );
+};
+
+export default Login;
---
+++ src/Page/Form/SignUp/SignUp.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const SignUp = () => {
+  return (
+    <div>
+      <h2>this is sign up section</h2>
+    </div>
+  );
+};
+
+export default SignUp;
