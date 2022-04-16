1b753eff6023fc5e50a8e84936816690be2ffee7
Minhazul Alam <78158511+akib-chy@users.noreply.github.com>
2022-04-16 20:17:42 +0600

    header section update

--- src/Shared/Header/Header.jsx
+++ src/Shared/Header/Header.jsx
@@ -1,9 +1,30 @@
 import React from "react";
+import logo from "../../images/logo2.png";
+import "./Header.css";
 
 const Header = () => {
   return (
-    <div>
-      <h2>this is header section</h2>
+    <div className="container d-flex justify-content-between align-items-center py-3">
+      <div>
+        <img className="nav-logo" src={logo} alt="" />
+      </div>
+      <div>
+        <div className="d-flex align-items-center">
+          <div>
+            <button className="border-0  bg-transparent mt-2">
+              <i className="fa-solid fa-cart-plus"></i>
+            </button>
+          </div>
+          <div className="ms-3 ">
+            <button className="border-0  bg-transparent fs-5">Login</button>
+          </div>
+          <div className="ms-3">
+            <button className="btn btn-danger rounded-pill  px-4">
+              Sign Up
+            </button>
+          </div>
+        </div>
+      </div>
     </div>
   );
 };
